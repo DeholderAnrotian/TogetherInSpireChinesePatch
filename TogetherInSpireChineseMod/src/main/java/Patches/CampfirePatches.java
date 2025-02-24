23417245d659afd4fc746bc34e587a3e7b8427cd
YeYuCheng666 <1104365195@qq.com>
2025-02-24 22:20:34 +0800

    Update CampfirePatches.java

--- TogetherInSpireChineseMod/src/main/java/Patches/CampfirePatches.java
+++ TogetherInSpireChineseMod/src/main/java/Patches/CampfirePatches.java
@@ -1,11 +1,9 @@
 package Patches;
 
-import basemod.ReflectionHacks;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
 import com.megacrit.cardcrawl.core.Settings;
 import dLib.util.Reflection;
-import spireTogether.SpireTogetherMod;
 import spireTogether.campfireOptions.ResurrectOption;
 
 public class CampfirePatches {
