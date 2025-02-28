90c608a1cfd54853538ddaef8d3235195ca25679
Deholder <yesxiaoduan@qq.com>
2025-03-01 01:29:12 +0800

    优化了怪物设置界面补丁的部分逻辑 添加了游戏设置界面的中文补丁

--- TogetherInSpireChineseMod/src/main/java/Patches/ScreenPatches/lobbyCP/GameSettingsScreenCP.java
+++ TogetherInSpireChineseMod/src/main/java/Patches/ScreenPatches/lobbyCP/GameSettingsScreenCP.java
@@ -1,10 +1,11 @@
 package Patches.ScreenPatches.lobbyCP;
 
 
-import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.evacipated.cardcrawl.modthespire.lib.*;
 import com.megacrit.cardcrawl.core.Settings;
 import dLib.util.Reflection;
 import spireTogether.screens.lobby.settings.GameSettingsScreen;
+import spireTogether.ui.elements.settings.ButtonUISetting;
 import spireTogether.ui.elements.settings.InputfieldUISetting;
 import spireTogether.ui.elements.settings.IntegerArrowUISetting;
 
@@ -23,14 +24,18 @@ public class GameSettingsScreenCP
       stringArray[3] = "初始荒疫";
       stringArray[4] = "初始药水";
       stringArray[5] = "回合时间限制";
-      int e = 5;
-      seed.label.text = stringArray[0];
-      for (int i = __instance.iterables.size() - 1; e > 0; i--)
-      {
-        IntegerArrowUISetting temp = (IntegerArrowUISetting) __instance.iterables.get(i).other.get(__instance.iterables.get(i).other.size() - 1);
-        temp.label.text = stringArray[e];
-        e--;
-      }
+      InputfieldUISetting temp0 = (InputfieldUISetting) __instance.iterables.get(0).other.get(0);
+      temp0.label.text = stringArray[0];
+      IntegerArrowUISetting temp1 = (IntegerArrowUISetting) __instance.iterables.get(1).other.get(0);
+      temp1.label.text = stringArray[1];
+      ButtonUISetting temp2 = (ButtonUISetting) __instance.iterables.get(2).middle;
+      temp2.label.text = stringArray[2];
+      ButtonUISetting temp3 = (ButtonUISetting) __instance.iterables.get(3).middle;
+      temp3.label.text = stringArray[3];
+      ButtonUISetting temp4 = (ButtonUISetting) __instance.iterables.get(4).middle;
+      temp4.label.text = stringArray[4];
+      IntegerArrowUISetting temp5 = (IntegerArrowUISetting) __instance.iterables.get(5).other.get(0);
+      temp5.label.text = stringArray[5];
     }
   }
 }
--- TogetherInSpireChineseMod/src/main/java/Patches/ScreenPatches/lobbyCP/MonsterSettingsScreenCP.java
+++ TogetherInSpireChineseMod/src/main/java/Patches/ScreenPatches/lobbyCP/MonsterSettingsScreenCP.java
@@ -1,13 +1,9 @@
 package Patches.ScreenPatches.lobbyCP;
 
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
-import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
 import com.megacrit.cardcrawl.core.Settings;
 import dLib.util.Reflection;
-import spireTogether.screens.Screen;
-import spireTogether.screens.lobby.MPHostPresetsScreen;
 import spireTogether.screens.lobby.settings.MonsterSettingsScreen;
-import spireTogether.ui.elements.UIElement;
 import spireTogether.ui.elements.settings.IntegerArrowUISetting;
 
 @SpirePatch(clz = spireTogether.screens.lobby.settings.MonsterSettingsScreen.class, method = "init", requiredModId = "spireTogether")
@@ -23,12 +19,10 @@ public class MonsterSettingsScreenCP
       stringArray[1] = "每名玩家使怪物血量上升值";
       stringArray[2] = "怪物基础伤害倍率";
       stringArray[3] = "每名玩家使怪物伤害上升值";
-      int e = 3;
-      for (int i = __instance.iterables.size() - 1; e >= 0; i--)
+      for (int i = 3; i >= 0; i--)
       {
-        IntegerArrowUISetting temp = (IntegerArrowUISetting) __instance.iterables.get(i).other.get(__instance.iterables.get(i).other.size() - 1);
-        temp.label.text = stringArray[e];
-        e--;
+        IntegerArrowUISetting temp = (IntegerArrowUISetting) __instance.iterables.get(i).other.get(0);
+        temp.label.text = stringArray[i];
       }
     }
   }
