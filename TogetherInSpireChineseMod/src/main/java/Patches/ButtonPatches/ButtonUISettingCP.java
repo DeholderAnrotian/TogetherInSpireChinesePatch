5eec9de8626d9129c30136fca3851f2911afeef4
Deholder <yesxiaoduan@qq.com>
2025-03-01 18:40:28 +0800

    添加了房间的游戏设置界面初始遗物药水荒疫的选择按钮中文

---
+++ TogetherInSpireChineseMod/src/main/java/Patches/ButtonPatches/ButtonUISettingCP.java
@@ -0,0 +1,22 @@
+package Patches.ButtonPatches;
+
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.megacrit.cardcrawl.core.Settings;
+import dLib.util.Reflection;
+import spireTogether.ui.elements.mixed.BoxedLabel;
+import spireTogether.ui.elements.settings.ButtonUISetting;
+
+public class ButtonUISettingCP
+{
+  @SpirePatch(clz = spireTogether.ui.elements.settings.ButtonUISetting.class, method = SpirePatch.CONSTRUCTOR, requiredModId = "spireTogether")
+  public static class constructorPostfix
+  {
+    public static void Postfix(ButtonUISetting __instance, String text, Integer xPos, Integer yPos)
+    {
+      if (Settings.language == Settings.GameLanguage.ZHS)
+      {
+        Reflection.setFieldValue("buttonLabel", __instance, new BoxedLabel("选择", xPos + 1478, yPos, 275, 75));
+      }
+    }
+  }
+}
