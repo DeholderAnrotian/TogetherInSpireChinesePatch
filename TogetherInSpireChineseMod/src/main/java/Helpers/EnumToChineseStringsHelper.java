fcb38ff125b49f32eccd9f8e952a7b398e21cf4b
Deholder <yesxiaoduan@qq.com>
2025-03-01 21:14:26 +0800

    将toChinese()方法增加对FriendlyFireType和StartType的重载

--- TogetherInSpireChineseMod/src/main/java/Helpers/EnumToChineseStringsHelper.java
+++ TogetherInSpireChineseMod/src/main/java/Helpers/EnumToChineseStringsHelper.java
@@ -27,4 +27,44 @@ public class EnumToChineseStringsHelper
       return "未知字符串";
     }
   }
+
+  public static String toChinese(GameSettings.StartType input)
+  {
+    if (input == GameSettings.StartType.FREE_FOR_ALL)
+    {
+      return "自由开始";
+    }
+    else if (input == GameSettings.StartType.OWNER_UNLOCK)
+    {
+      return "房主解锁后自由开始";
+    }
+    else if (input == GameSettings.StartType.ALL_READY)
+    {
+      return "全部准备后开始";
+    }
+    else
+    {
+      return "未知字符串";
+    }
+  }
+
+  public static String toChinese(GameSettings.FriendlyFireType input)
+  {
+    if (input == GameSettings.FriendlyFireType.FULL)
+    {
+      return "全部";
+    }
+    else if (input == GameSettings.FriendlyFireType.NONE)
+    {
+      return "无友伤";
+    }
+    else if (input == GameSettings.FriendlyFireType.TARGETING)
+    {
+      return "指向";
+    }
+    else
+    {
+      return "未知字符串";
+    }
+  }
 }
